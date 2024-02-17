5b8e314a9e0a78e84a02feac662d65aa062644b3
rune_gamer rune_gamer <rune00nielsen@gmail.com>
2024-02-17 19:49:54 +0100

    Add project files.

---
+++ wynncraftAttackSpot/Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+using wynncraftAttackSpot;
+using Newtonsoft.Json;
+
+List<String> list = new List<String>();
+HttpClient HttpClient= new();
+
+HttpResponseMessage FirstResponse = HttpClient.GetAsync("https://api.wynncraft.com/v3/guild/list/territory").Result;
+var FirstTerrRequest = JsonConvert.DeserializeObject<Dictionary<string,Territory>>(FirstResponse.Content.ReadAsStringAsync().Result);
+foreach(KeyValuePair<string,Territory> kvp in FirstTerrRequest)
+{
+    if (kvp.Value.guild.name.Contains("Idiot")) { list.Add(kvp.Key); }
+}
+
+bool start = false;
+Thread UI = new(UserInput);
+UI.Start();
+while (true)
+{
+    if (start)
+    { // https://api.wynncraft.com/v3/guild/list/territory
+        HttpResponseMessage httpResponse = HttpClient.GetAsync("https://api.wynncraft.com/v3/guild/list/territory").Result;
+        try
+        {
+            httpResponse.EnsureSuccessStatusCode();
+            Console.WriteLine(httpResponse.Content.ReadAsStringAsync().Result.Length);
+            string result = httpResponse.Content.ReadAsStringAsync().Result;
+            Dictionary<string,Territory> territoryRequest = JsonConvert.DeserializeObject<Dictionary<string,Territory>>(result) ?? new();
+            foreach(string key in list)
+            {
+                if (!territoryRequest[key].guild.name.Contains("Idiot"))
+                {
+                    Console.WriteLine("Reclaim Reclaim Reclaim");
+                    break;
+                }
+            }
+        } catch (HttpRequestException)
+        {
+            Console.WriteLine("dead");
+            break;
+        }
+        Thread.Sleep(30000);
+    }
+}
+void UserInput()
+{
+    Console.WriteLine("Press 'a' to start");
+    while (true)
+    {
+        switch (Console.ReadKey().Key)
+        {
+            case ConsoleKey.A: 
+                start = true;
+                Console.Clear();
+                break;
+            case ConsoleKey.Escape:
+                Environment.Exit(0);
+                break;
+            case ConsoleKey.P:
+                start = false;
+                break;
+        }
+        Console.WriteLine("Press 'a' to start");
+    }
+}
---
+++ wynncraftAttackSpot/Territory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wynncraftAttackSpot
+{
+    public class Territory
+    {
+        public string territory { get; set; }
+        public guild guild { get; set; }
+        public DateTime acquired { get; set; }
+        //"guild": {
+        //      "uuid": str,
+        //      "name": str,
+        //      "prefix": str
+    }
+
+    public class guild
+    {
+        public string name { get; set; }
+    }
+}
